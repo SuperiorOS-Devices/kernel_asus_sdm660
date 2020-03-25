Sanju-Thomas/android_kernel_asus_sdm660

msm: mdss: Import X01BD changes
Signed-off-by: Rahul Krishna <Rahulkrishna585@gmail.com>

drivers/video/fbdev/msm/mdss_dsi_status.c
@@ -30,8 +30,10 @@
 #include "mdss_panel.h"
 #include "mdss_mdp.h"
 
-#ifdef CONFIG_MACH_ASUS_X00TD
+#if defined(CONFIG_MACH_ASUS_X00TD)
 #define STATUS_CHECK_INTERVAL_MS 500
+#elseif defined(CONFIG_MACH_ASUS_X01BD)
+#define STATUS_CHECK_INTERVAL_MS 2000
 #else
 #define STATUS_CHECK_INTERVAL_MS 5000
 #endif

SuperiorOS-Devices/kernel_asus_sdm660

drivers/video/fbdev/msm/mdss_dsi_status.c
@@ -30,8 +30,10 @@
 #include "mdss_panel.h"
 #include "mdss_mdp.h"
 
-#ifdef CONFIG_MACH_ASUS_X00TD
+#if defined(CONFIG_MACH_ASUS_X00TD)
 #define STATUS_CHECK_INTERVAL_MS 500
+#elif defined(CONFIG_MACH_ASUS_X01BD)
+#define STATUS_CHECK_INTERVAL_MS 2000
 #else
 #define STATUS_CHECK_INTERVAL_MS 5000
 #endif