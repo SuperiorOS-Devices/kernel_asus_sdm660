4563cdad205d187360a2f387409cb5a19e2c28a6
shibierr404 <shibisbs123@gmail.com>
2021-06-27 07:21:20 +0530

    Rectify Conflict

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -145,12 +145,7 @@ struct gadget_config_name {
 	struct list_head list;
 };
 
-<<<<<<< HEAD
-#define MAX_USB_STRING_LEN	126
 #define MAX_USB_STRING_WITH_NULL_LEN	(MAX_USB_STRING_LEN+1)
-=======
-#define USB_MAX_STRING_WITH_NULL_LEN	(USB_MAX_STRING_LEN+1)
->>>>>>> 28a3d5b101b3adfe4eef230ead62d824a103199e
 
 static int usb_string_copy(const char *s, char **s_copy)
 {
@@ -158,13 +153,13 @@ static int usb_string_copy(const char *s, char **s_copy)
 	char *str;
 	char *copy = *s_copy;
 	ret = strlen(s);
-	if (ret > MAX_USB_STRING_LEN)
+	if (ret > USB_MAX_STRING_LEN)
 		return -EOVERFLOW;
 
 	if (copy) {
 		str = copy;
 	} else {
-		str = kmalloc(MAX_USB_STRING_WITH_NULL_LEN, GFP_KERNEL);
+		str = kmalloc(USB_MAX_STRING_WITH_NULL_LEN, GFP_KERNEL);
 		if (!str)
 			return -ENOMEM;
 	}
